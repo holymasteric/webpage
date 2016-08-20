8229a9cd764b65ab5fa81e6afa67fef25700d765
ericlaw <ld_0705@yahoo.com>
2016-08-20 12:48:33 -0700

    Initial commit with gitignore file

---
+++ travelmap/travel-geojson.js
@@ -0,0 +1,410 @@
+var travel =
+{
+   "features" : [
+      {
+         "geometry" : {
+            "coordinates" : [
+            	114.109500,22.396400 
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Hong Kong, China",
+            "Image" : "images/hong_kong.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+            	-122.676500,45.523100 
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Portland, OR",
+            "Image" : "images/portland.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+            	121.565400,25.033000 
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Taipei, Taiwan",
+            "Image" : "images/taipei.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -157.858300,21.306900
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Honolulu, HI",
+            "Image" : "images/honolulu.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -119.813800,39.529600
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Reno, NV",
+            "Image" : "images/reno.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -123.805300,39.445700 
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Fort Bragg, CA",
+            "Image" : "images/fort_bragg.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -81.379200,28.538300
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Orlando, FL",
+            "Image" : "images/orlando.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -115.139800,36.169900
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Las Vegas, NV",
+            "Image" : "images/las_vegas.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -118.243700,34.052200
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Los Angeles, CA",
+            "Image" : "images/los_angeles.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -117.161100,32.715700
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "San Digeo, CA",
+            "Image" : "images/san_diego.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -116.596400,31.866700
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Enseñada, Mexico",
+            "Image" : "images/ensenada.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -118.416300,33.387900
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Catalina Island, CA",
+            "Image" : "images/catalina_island.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               113.543900,22.198700
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Macau, China",
+            "Image" : "images/macau.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -122.419400,37.774900
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "San Francisco, CA",
+            "Image" : "images/san_francisco.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -121.494400,38.581600
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Sacramento, CA",
+            "Image" : "images/sacramento.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -120.183300,39.328000
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Truckee, CA",
+            "Image" : "images/truckee.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -120.183300,42.326500
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Medford, OR",
+            "Image" : "images/medford.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -123.328400,42.439000
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Grants Pass, OR",
+            "Image" : "images/grants_pass.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -122.391700,40.586500
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Redding, CA",
+            "Image" : "images/redding.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -122.168500,42.868400
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Crater Lake, OR",
+            "Image" : "images/crater_lake.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+                -124.202600,41.755800
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Crescent City, CA",
+            "Image" : "images/crescent_city.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -123.799500,39.307700
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Mendocino, CA",
+            "Image" : "images/mendocino.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -121.894700,36.600200
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Monterey, CA",
+            "Image" : "images/monterey.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -122.030800,36.974100
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Santa Cruz, CA",
+            "Image" : "images/santa_cruz.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -120.659600,35.282800
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "San Luis Obispo, CA",
+            "Image" : "images/san_luis_obispo.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -119.698200,34.420800
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Santa Barbara, CA",
+            "Image" : "images/santa_barbara.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -117.914500,33.835300
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Anaheim, CA",
+            "Image" : "images/anaheim.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -117.350600,33.158100
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Carlsbad, CA",
+            "Image" : "images/carlsbad.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -117.794700,33.683900
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Irvine, CA",
+            "Image" : "images/irvine.jpg"
+         },
+         "type" : "Feature"
+      },
+      {
+         "geometry" : {
+            "coordinates" : [
+               -117.396200,33.953300
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Riverside, CA",
+            "Image" : "images/riverside.jpg"
+         },
+         "type" : "Feature"
+      }, 
+      {
+         "geometry" : {
+            "coordinates" : [
+               -157.804100,21.263800
+            ],
+            "type" : "Point"
+         },
+         "properties" : {
+            "City" : "Diamond Head, HI",
+            "Image" : "images/diamond_head.jpg"
+         },
+         "type" : "Feature"
+      }
+
+   ],
+   "type" : "FeatureCollection"
+} 
